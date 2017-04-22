f1093f9b17252b07830fd3474cd932901088a693
Kev Adsett <kev.adsett@gmail.com>
2017-04-22 22:17:04 +0100

    Add npc state machine

---
+++ Congamoeba/Assets/Scripts/StateMachine/GameStates/NpcStates/IdlingState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Congamoeba.GameStateMachine;
+using UnityEngine;
+
+namespace Congamoeba.NPC
+{
+	public class IdlingState : IGameState
+	{
+		public Camera StateCamera { get { return null; } }
+
+		private GameObject _npc;
+
+		public IdlingState (GameObject npc)
+		{
+			_npc = npc;
+		}
+
+		public void OnEnter()
+		{
+		}
+
+		public void Update ()
+		{
+		}
+
+		public void OnExit ()
+		{
+		}
+	}
+}
+
---
+++ Congamoeba/Assets/Scripts/StateMachine/NpcStateMachine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Congamoeba.GameStateMachine;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Congamoeba.NPC
+{
+	public enum eNpcState
+	{
+		Idling
+	};
+
+	public class NpcStateMachine : MonoBehaviour
+	{
+		public Dictionary<eNpcState, IGameState> _states;
+
+		void Awake()
+		{
+			_states = new Dictionary<eNpcState, IGameState> {
+				{ eNpcState.Idling, new IdlingState (gameObject) }
+			};
+		}
+	}
+}
+
