c43e72fb88dcdc07cf02fee718fb46d261f6ea5f
Kev Adsett <kev.adsett@gmail.com>
2017-04-22 21:55:31 +0100

    Add NPC generation

---
+++ Congamoeba/Assets/Scripts/NPC/NpcGeneration.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class NpcGeneration : MonoBehaviour {
+	public GameObject NpcPrefab;
+
+	public List<int> GenerationBands;
+
+	public float DistanceBetweenBands;
+
+	public float Randomisation;
+
+	private int _totalNpcsToCreate;
+	private int _totalCreatedNpcs;
+
+	void Start ()
+	{
+		for (int i = 0; i < GenerationBands.Count; i++)
+		{
+			_totalNpcsToCreate += GenerationBands[i];
+		}
+	}
+
+	void Update ()
+	{
+		if (_totalCreatedNpcs < _totalNpcsToCreate)
+		{
+			for (int i = 0; i < GenerationBands.Count; i++)
+			{
+				int npcCount = GenerationBands [i];
+				float bandDistance = (i + 1) * DistanceBetweenBands;
+
+				float step = ((Mathf.PI * 2f) / npcCount);
+
+				float offset = Random.value * Mathf.PI * 2f;
+				for (int j = 0; j < npcCount; j++)
+				{
+					float angle = offset + step * j;
+					float x = bandDistance * Mathf.Cos (angle) + ((Random.value * 2) - 1) * Randomisation;
+					float y = bandDistance * Mathf.Sin (angle) + ((Random.value * 2) - 1) * Randomisation;
+
+					GameObject npc = GameObject.Instantiate (NpcPrefab);
+					npc.transform.position = new Vector3 (x, y, npc.transform.position.z);
+					_totalCreatedNpcs++;
+				}
+			}
+		}
+	}
+}
