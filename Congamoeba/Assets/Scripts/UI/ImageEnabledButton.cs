9203b4ff5bf0f8047fa4acbce62aaddc344a03b9
gav <gavin@bossalien.com>
2017-04-23 17:09:09 +0100

    basics of kb ui support

---
+++ Congamoeba/Assets/Scripts/UI/ImageEnabledButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent (typeof (MaskableGraphic))]
+public class ImageEnabledButton : MonoBehaviour {
+	[SerializeField] string button;
+	[SerializeField] float duration;
+
+	MaskableGraphic graphic;
+	Color color;
+	float time;
+
+	void Awake () {
+		graphic = GetComponent<MaskableGraphic> ();
+		color = graphic.color;
+	}
+
+	void Update () {
+		time -= Time.deltaTime / duration;
+
+		if (Input.GetButton (button)) {
+			time = 1f;
+		}
+
+		color.a = Mathf.Clamp01 (time);
+		graphic.color = color;
+	}
+}
