77176e884c65efa35168d9472542a3ea80db913d
Kev Adsett <kev.adsett@gmail.com>
2017-04-23 07:00:05 +0100

    Reduce npc movement speed, but allow any direction

--- Congamoeba/Assets/Scripts/StateMachine/GameStates/NpcStates/IdlingState.cs
+++ Congamoeba/Assets/Scripts/StateMachine/GameStates/NpcStates/IdlingState.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using Congamoeba.GameStateMachine;
+﻿using Congamoeba.GameStateMachine;
 using UnityEngine;
+using Congamoeba.Player;
 
 namespace Congamoeba.NPC
 {
@@ -10,9 +10,19 @@ namespace Congamoeba.NPC
 
 		private GameObject _npc;
 
+		private PlayerPhysics _npcPhysics;
+//		private NpcStateMachine _stateMachine;
+
+		private const float MINIMUM_DIRECTION_DURATION = 0f;
+
+		private const float MAX_SPEED = 0.01f;
+
 		public IdlingState (GameObject npc)
 		{
 			_npc = npc;
+			_npcPhysics = _npc.GetComponent<PlayerPhysics> ();
+			_npcPhysics.Acceleration = Random.insideUnitCircle * MAX_SPEED;
+//			_stateMachine = npc.GetComponent<NpcStateMachine> ();
 		}
 
 		public void OnEnter()
@@ -21,6 +31,10 @@ namespace Congamoeba.NPC
 
 		public void Update ()
 		{
+			float xChange = ((Random.value * 2) - 1) * MAX_SPEED / 10;
+			float yChange = ((Random.value * 2) - 1) * MAX_SPEED / 10;
+			_npcPhysics.Acceleration.x = Mathf.Clamp (_npcPhysics.Acceleration.x + xChange, -MAX_SPEED, MAX_SPEED);
+			_npcPhysics.Acceleration.y = Mathf.Clamp (_npcPhysics.Acceleration.y + yChange, -MAX_SPEED, MAX_SPEED);
 		}
 
 		public void OnExit ()
--- Congamoeba/Assets/Scripts/StateMachine/NpcStateMachine.cs
+++ Congamoeba/Assets/Scripts/StateMachine/NpcStateMachine.cs
@@ -7,18 +7,41 @@ namespace Congamoeba.NPC
 {
 	public enum eNpcState
 	{
-		Idling
+		Idling,
+		Talking,
+		Listening,
+		Reacting,
+		Following,
+		Merging
 	};
 
 	public class NpcStateMachine : MonoBehaviour
 	{
 		public Dictionary<eNpcState, IGameState> _states;
 
+		private IGameState _currentState;
+
 		void Awake()
 		{
 			_states = new Dictionary<eNpcState, IGameState> {
 				{ eNpcState.Idling, new IdlingState (gameObject) }
 			};
+			ChangeState (eNpcState.Idling);
+		}
+
+		void ChangeState(eNpcState newState)
+		{
+			if (_currentState != null)
+			{
+				_currentState.OnExit ();
+			}
+			_currentState = _states [newState];
+			_currentState.OnEnter ();
+		}
+
+		void Update()
+		{
+			_currentState.Update ();
 		}
 	}
 }
