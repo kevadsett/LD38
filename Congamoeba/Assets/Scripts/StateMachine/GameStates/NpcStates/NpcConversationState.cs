5d3cb3bbd39a46542138fe73e87d6e35e946d397
Kev Adsett <kev.adsett@gmail.com>
2017-04-24 22:39:08 +0100

    Comment out unassigned variable.

--- Congamoeba/Assets/Scripts/StateMachine/GameStates/NpcStates/NpcConversationState.cs
+++ Congamoeba/Assets/Scripts/StateMachine/GameStates/NpcStates/NpcConversationState.cs
@@ -110,7 +110,7 @@ namespace Congamoeba.NPC
 					_audioSource.clip = ConversationService.GetNpcClip (_yaySound.name, _stateMachine.VoiceSettings);
 					_stateMachine.ChangeState (eNpcState.Following);
 					_playerSounds.PlaySuccess ();
-					Vector3 scale = _playerSounds.gameObject.transform.localScale;
+//					Vector3 scale = _playerSounds.gameObject.transform.localScale;
 //					_playerSounds.gameObject.transform.localScale = new Vector3 (scale.x * 1.5f, scale.y * 1.5f, scale.z);
 					break;
 				case eReactionType.nay:
